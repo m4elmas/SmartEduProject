e660f428ea7667524ad85c8a19196cc083a7c6f8
Mucahit <mucahit.elmas@hotmail.com>
2023-08-27 11:39:36 +0300

    ikinci ders

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ const app = express();
 
 
 
+
+//Template Engine
+app.set("view engine","ejs")
+
+
+//MiddleWares
+app.use(express.static("public"))
+
+
 //ROUTES
 app.get("/",(req,res)=>{
-    res.send("lol")
+    res.status(200).render("index",{
+        page_name : "index"
+    })
+})
+app.get("/about",(req,res)=>{
+    res.status(200).render("about",{
+        page_name : "about"
+    })
 })
 
 
